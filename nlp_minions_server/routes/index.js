200695c3f551e666280e8c48194d03b76dcd6aa2
ChoHadam <hadamcho97@gmail.com>
2020-09-10 11:27:02 +0900

    index.js 내용 수정, gitignore 복구

--- nlp_minions_server/routes/index.js
+++ nlp_minions_server/routes/index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router({mergeParams: true});
 
 router.use('/data', require('./Data'));
+router.use('/user', require('./User'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('"인스타그램 데이터 구축 프로젝트" 사이트에 오신걸 환영합니다!<br><br><br>- NLP-Minions Fam👨‍👧‍👧 : 승범 은혜 하담 규명 인용');
+  res.send('"인스타그램 데이터 구축 프로젝트" 사이트에 오신걸 환영합니다!<br><br><br>- NLP-Minions Fam👨‍👧‍👧 : 승범 은혜 하담 규명 인용 지상 소정');
 });
 
 module.exports = router;
