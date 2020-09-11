0b9d0406ab8b5aed14a2088f4e0d43b3bfc7bd20
ChoHadam <hadamcho97@gmail.com>
2020-09-11 15:15:35 +0900

    index.js에 cors 허용 헤더 추가

--- nlp_minions_server/routes/index.js
+++ nlp_minions_server/routes/index.js
@@ -6,6 +6,7 @@ router.use('/user', require('./User'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.send('"인스타그램 데이터 구축 프로젝트" 사이트에 오신걸 환영합니다!<br><br><br>- NLP-Minions Fam👨‍👧‍👧 : 승범 은혜 하담 규명 인용 지상 소정');
 });
 
