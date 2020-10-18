6e8ed2776b2232434ecf020b6bf3cfd5c9c914bf
ehhahappy <mqmqmqmqmq@naver.com>
2020-10-19 07:15:58 +0900

    add_profile, setting_query

---
+++ nlp_minions_database/SQL_query/setting_query.sql
@@ -0,0 +1,43 @@
+SELECT * FROM SocialAnalysis.POST_4;
+
+ALTER DATABASE SocialAnalysis CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+ALTER TABLE POST CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE POST modify content text charset utf8mb4;
+
+ALTER TABLE POST CHANGE content content VARCHAR(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+TRUNCATE TABLE INSTA_RELATION_2;
+
+TRUNCATE TABLE INSTA_PROFILE_3;
+
+DROP TABLE REPLY;
+
+INSERT INTO POST (SELECT * FROM POST_4);
+
+INSERT INTO POST(shortcode, inner_id, post_date, content, team_idx) 
+SELECT shortcode, inner_id, post_date, content, team_idx
+FROM POST_1;
+
+INSERT IGNORE INTO POST(shortcode, inner_id, post_date, content, team_idx) 
+SELECT shortcode, inner_id, post_date, content, team_idx
+FROM POST_4;
+
+INSERT INTO INSTA_RELATION (SELECT * FROM INSTA_RELATION_4);
+
+INSERT INTO INSTA_RELATION(start, end, relation_type, team_idx) 
+SELECT start, end, relation_type, team_idx
+FROM INSTA_RELATION_3;
+
+
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM SocialAnalysis.POST_4 WHERE team_idx=1;
+
+INSERT IGNORE INTO INSTA_PROFILE(inner_id, team_idx) 
+SELECT inner_id, team_idx
+FROM POST;
+
+ALTER TABLE POST ADD FOREIGN KEY (inner_id) REFERENCES INSTA_PROFILE(inner_id);
+
+UPDATE INSTA_PROFILE SET insta_id = null WHERE inner_id = '10007242076'
