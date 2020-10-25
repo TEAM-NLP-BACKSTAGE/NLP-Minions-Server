f5647025888e38d48b1ae19a1364d00a05c079f7
ehhahappy <mqmqmqmqmq@naver.com>
2020-10-25 22:02:02 +0900

    Set post_idx rearrange

--- nlp_minions_database/SQL_query/setting_query.sql
+++ nlp_minions_database/SQL_query/setting_query.sql
@@ -12,12 +12,14 @@ TRUNCATE TABLE INSTA_RELATION_2;
 
 TRUNCATE TABLE INSTA_PROFILE_3;
 
+TRUNCATE TABLE LABEL;
+
 DROP TABLE REPLY;
 
 INSERT INTO POST (SELECT * FROM POST_4);
 
-INSERT INTO POST(shortcode, inner_id, post_date, content, team_idx) 
-SELECT shortcode, inner_id, post_date, content, team_idx
+INSERT INTO POST(shortcode, inner_id, post_date, crawling_time, like_count, view_count, url, media_url, content, region_tag, hashtag, team_idx) 
+SELECT shortcode, inner_id, post_date, crawling_time, like_count, view_count, url, media_url, content, region_tag, hashtag, team_idx
 FROM POST_1;
 
 INSERT IGNORE INTO POST(shortcode, inner_id, post_date, content, team_idx) 
@@ -32,7 +34,8 @@ FROM INSTA_RELATION_3;
 
 
 SET SQL_SAFE_UPDATES = 0;
-DELETE FROM SocialAnalysis.POST_4 WHERE team_idx=1;
+
+DELETE FROM POST WHERE team_idx=1;
 
 INSERT IGNORE INTO INSTA_PROFILE(inner_id, team_idx) 
 SELECT inner_id, team_idx
@@ -41,3 +44,7 @@ FROM POST;
 ALTER TABLE POST ADD FOREIGN KEY (inner_id) REFERENCES INSTA_PROFILE(inner_id);
 
 UPDATE INSTA_PROFILE SET insta_id = null WHERE inner_id = '10007242076'
+
+ALTER TABLE POST AUTO_INCREMENT=1;
+SET @COUNT = 0;
+UPDATE POST SET post_idx = @COUNT:=@COUNT+1;
